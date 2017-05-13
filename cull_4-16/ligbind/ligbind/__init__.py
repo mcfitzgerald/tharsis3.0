217510bdf442064887ca113cbdef6db77a100c8d
mcfitzgerald <michael.craig.fitzgerald@gmail.com>
2017-05-13 12:03:49 -0400

    piglet

--- cull_4-16/ligbind/ligbind/__init__.py
+++ cull_4-16/ligbind/ligbind/__init__.py
@@ -1,4 +1,10 @@
+"""ligbind - Tools to simulate and fit ligbind binding isotherms"""
+
+__version__ = '0.1.0'
+__author__ = 'Michael Fitzgerald <michael.craig.fitzgerald@gmail.com'
+
 from .core import *
 from .models import *
 from .helpers import *
 from .plotting import *
+from .prep import *
